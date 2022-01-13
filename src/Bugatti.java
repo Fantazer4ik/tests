c70681c401e22cfb80ffe2826d1868d982846a79
Sanya <fantazer4ik@gmail.com>
2022-01-13 12:21:49 +0200

    JavaRush 11/5 Equals

---
+++ src/Bugatti.java
@@ -0,0 +1,27 @@
+/*
+Ти зробив попереднє замовлення на новеньку Bugatti рівно пів року тому.
+Зараз червень, саме час їздити на кабріолеті. Але ти забув, який тип кузова вибрав під час замовлення.
+Твоє завдання — додати функціональність для отримання поточної конфігурації та для її зміни в разі потреби.
+Для цього створи гетери й сетери.
+
+Требования:
+Клас Bugatti має містити три поля: String color, int year, String body.
+Клас Bugatti має містити публічний гетер getBody для поля body.
+Клас Bugatti має містити публічний сетер setBody(String) для поля body.
+ */
+
+public class Bugatti {
+    private String color = "BLACK";
+    private int year = 2020;
+    private String body = "Coupe";
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+
+
+    }
+}
